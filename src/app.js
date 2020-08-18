6bd95dcea960f888d4a2395d1110147432bbd931
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-18 11:46:55 -0300

    The application should be able to list the repositories

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (req, res) => {
-  return res.status(200).json({ repositories })
+  return res.status(200).send([ repositories ])
 });
 
 app.post("/repositories", (req, res) => {
